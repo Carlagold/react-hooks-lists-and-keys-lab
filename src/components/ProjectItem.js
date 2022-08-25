8f703cf5d9f17c633cf60f61c6694804019445ad
Carla <csachett@student.scf.edu>
2022-08-25 15:26:48 -0400

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologiesArr = technologies.map((project) => (
+    <span key={project}>{project}</span>
+  ));
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologiesArr}</div>
     </div>
   );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,41 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const projectItems = projects.map((project) => (
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ));
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItems}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({projects}) { 
+//    for (const project of projects) {
+//     <ProjectItem 
+//     key={project.id} 
+//     name={project.name} 
+//     about={project.about} 
+//     technologies={project.technologies} />
+//   };
+//     return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">{ProjectItem}</div>
+//     </div>
+//   );
+// }
+    
+// export default <ProjectList projects={name} />;
